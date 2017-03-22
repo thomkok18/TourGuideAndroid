7e2417208bc71519d6838f937fb34237a56506f4
Thom Kok <tkok@roc-dev.com>
2017-03-22 12:44:25 +0100

    update

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.tourguide;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.TabLayout;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void naam(View view) {
+        Uri uri = Uri.parse("http://www.google.com/#q=Citymall Almere, Forum 101, 1315 TG Almere");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
